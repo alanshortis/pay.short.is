e98843067dc90967f07bb1fa583206a2a2e7f407
Alan Shortis <alan.shortis@mac.com>
2018-09-29 19:26:33 +0100

    New version, using create react app

---
+++ src/components/Pay.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Pay extends Component {
+  render() {
+    return <div>Pay.</div>;
+  }
+}
+
+export default Pay;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pay from "./components/Pay";
+
+ReactDOM.render(<Pay />, document.getElementById("root"));
