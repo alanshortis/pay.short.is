2b3ddf7528f5e82bfc285abada8e1f3a7e1277dc
Alan Shortis <alan.shortis@mac.com>
2018-09-29 22:47:14 +0100

    Show providers, if they can handle the amount requested

--- src/components/Pay.js
+++ src/components/Pay.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { formatNumbers } from "../utils";
+import providers from "../data/providers";
+import Provider from "./Provider";
 
 class Pay extends Component {
   constructor(props) {
@@ -7,11 +9,24 @@ class Pay extends Component {
 
     this.state = formatNumbers(this.props.match.params.amount);
     this.props.history.push(`/${this.state.amount}`);
+
+    this.validProviders = providers.filter(
+      ({ min, max }) =>
+        (min === 0 || this.state.amount >= min) &&
+        (max === 0 || this.state.amount <= max)
+    );
   }
 
   render() {
-    const { displayAmount } = this.state;
-    return <div>{displayAmount}</div>;
+    const { displayAmount, amount } = this.state;
+    return (
+      <div>
+        <h1>{displayAmount}</h1>
+        {this.validProviders.map(provider => (
+          <Provider key={provider.id} provider={provider} amount={amount} />
+        ))}
+      </div>
+    );
   }
 }
 
---
+++ src/components/Provider.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Provider = ({ amount, provider }) => {
+  return <a href={`${provider.url}${amount}`}>{provider.name}</a>;
+};
+
+export default Provider;
