c0526e9db7069d3c52e96600e92a7fda3e13e08d
Alan Shortis <alan.shortis@mac.com>
2018-10-01 09:30:06 +0100

    The new stuff isn't always the best stuff

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Provider = ({ amount, provider }) => {
-  return <a href={`${provider.url}${amount}`}>{provider.name}</a>;
+  return <a href={provider.url + amount}>{provider.name}</a>;
 };
 
 export default Provider;
