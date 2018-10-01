e14e3496761a109a1100d3d65afeb494ef246f2b
Alan Shortis <alan.shortis@mac.com>
2018-10-01 14:40:36 +0100

    Check prop types for provider

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,7 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Provider = ({ amount, provider }) => {
   return <a href={provider.url + amount}>{provider.name}</a>;
+};
+
+Provider.propTypes = {
+  amount: PropTypes.string.isRequired,
+  provider: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  })
 };
 
 export default Provider;
