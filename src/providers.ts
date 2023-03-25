52173a230be754c92b874685f570252862887680
Alan Shortis <alan.shortis@mac.com>
2023-03-25 19:13:12 +0000

    Set up next

---
+++ src/providers.ts
@@ -0,0 +1,14 @@
+export const providers = [
+  {
+    name: 'PayPal',
+    url: 'https://paypal.me/shortis/',
+    min: 0,
+    max: 0,
+  },
+  {
+    name: 'Monzo',
+    url: 'https://monzo.me/alanshortis/',
+    min: 1,
+    max: 500,
+  },
+];
